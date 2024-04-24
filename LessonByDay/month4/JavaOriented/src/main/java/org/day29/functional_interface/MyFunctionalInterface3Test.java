806c8555a2ee495b2c5fbcd9ee043b53a9f6c07b
minjikimkim2222 <olivia0929@naver.com>
2024-04-24 11:45:15 +0900

    0424 - lambda ex 실습들

--- LessonByDay/month4/JavaOriented/src/main/java/org/day29/functional_interface/MyFunctionalInterface3Test.java
+++ LessonByDay/month4/JavaOriented/src/main/java/org/day29/functional_interface/MyFunctionalInterface3Test.java
@@ -1,6 +1,9 @@
 package org.day29.functional_interface;
 
 public class MyFunctionalInterface3Test {
+    public static int minus(int x, int y){
+        return x-y;
+    }
     public static void main(String[] args) {
         // 1. 익명객체
         MyFunctionInterface3 inter2 = new MyFunctionInterface3() {
@@ -11,12 +14,19 @@ public class MyFunctionalInterface3Test {
         };
         System.out.println(inter2.sum(3,4));
 
-        // 2. 람다식
+        // 2. 람다식 - interface 사용
         MyFunctionInterface3 inter3;
 
         inter3 = (x,y)-> {return x+y;};
 
         System.out.println(inter3.sum(2,3));
+
+        // 3. 람다식 - minus static 함수 사용
+        MyFunctionInterface3 inter4;
+
+        inter4 = (x,y) -> {return minus(x,y);};
+
+        System.out.println(inter4.sum(2,3)); // 실제로는 2-3 == -1
 
     }
 }
