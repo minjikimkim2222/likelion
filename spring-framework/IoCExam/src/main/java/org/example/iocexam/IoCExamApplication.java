e947be0fc6a7b0354a9160973ae821f77545f296
minjikimkim2222 <olivia0929@naver.com>
2024-05-10 10:05:52 +0900

    0510_demo프로젝트 삭제_IoCExam 프로젝트 만들기

---
+++ spring-framework/IoCExam/src/main/java/org/example/iocexam/IoCExamApplication.java
@@ -0,0 +1,13 @@
+package org.example.iocexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoCExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoCExamApplication.class, args);
+    }
+
+}
--- spring-framework/IoCExam/src/test/java/org/example/iocexam/IoCExamApplicationTests.java
+++ spring-framework/IoCExam/src/test/java/org/example/iocexam/IoCExamApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package org.example.iocexam;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class IoCExamApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
