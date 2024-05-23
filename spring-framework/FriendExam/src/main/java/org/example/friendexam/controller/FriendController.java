0b23c6fa1f4fb95bb6cd7ee92a570d825e57bf57
minjikimkim2222 <olivia0929@naver.com>
2024-05-23 16:08:59 +0900

    0523_CRUD, 페이징 실습예제

--- spring-framework/FriendExam/src/main/java/org/example/friendexam/controller/FriendController.java
+++ spring-framework/FriendExam/src/main/java/org/example/friendexam/controller/FriendController.java
@@ -3,6 +3,9 @@ package org.example.friendexam.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.friendexam.domain.Friend;
 import org.example.friendexam.service.FriendService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -14,10 +17,22 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class FriendController {
     private final FriendService friendService;
 
+//    @GetMapping
+//    public String showFriendsList(Model model){
+//        Iterable<Friend> friends = friendService.findAllFriends();
+//        model.addAttribute("friends", friends);
+//        return "friends/list"; // friends 폴더 밑의 list.html 뷰
+//    }
+
     @GetMapping
-    public String showFriendsList(Model model){
-        Iterable<Friend> friends = friendService.findAllFriends();
+    public String showFriendsList(Model model,
+          @RequestParam(defaultValue = "1")int page,
+          @RequestParam(defaultValue = "5") int size){
+        Pageable pageable = PageRequest.of(page-1, size);
+
+        Page<Friend> friends = friendService.findAllFriendsWithPage(pageable);
         model.addAttribute("friends", friends);
+        model.addAttribute("currentPage", page); // 현재 페이지 값도 보내준다.
         return "friends/list"; // friends 폴더 밑의 list.html 뷰
     }
 
@@ -39,11 +54,35 @@ public class FriendController {
         return "redirect:/friends";
     }
 
-    // 친구수정기능 -- url 마찬가지로 2개
+    // 특정 id에 해당하는 친구 조회
     @GetMapping("/{id}")
-    public String editFriend(@PathVariable long id, Model model){
+    public String detailFriend(@PathVariable long id, Model model){
         Friend editFriend = friendService.findFriendById(id);
         model.addAttribute("editFriend", editFriend);
-        return "friends/edit";
+        return "friends/detail";
     }
+
+    // delete기능
+    @GetMapping("/delete/{id}")
+    public String deleteFriend(@PathVariable long id){
+        friendService.deleteFriendById(id);
+        return "redirect:/friends"; // 다시 친구 목록으로..
+    }
+
+    // edit 기능 -- url 2개 필요, 수정 폼 좀 보여줘.. 수정 해줘(re)
+    // localhost/friends/edit/{id} -- GET 수정폼
+    // localhost/friends/edit/{id} -- Post 수정
+    @GetMapping("/edit/{id}")
+    public String editForm(@PathVariable Long id, Model model){
+        Friend friend = friendService.findFriendById(id); // 수정 대상 friend
+        model.addAttribute("friend", friend);
+        return "/friends/edit";
+    }
+
+    @PostMapping("/edit")
+    public String editFriend(@ModelAttribute("friend") Friend friend){
+        friendService.saveFriend(friend); // save는 해당 객체가 없으면 insert, 있다면 update!!
+        return "redirect:/friends";
+    }
+
 }
--- spring-framework/FriendExam/src/main/java/org/example/friendexam/service/FriendService.java
+++ spring-framework/FriendExam/src/main/java/org/example/friendexam/service/FriendService.java
@@ -3,6 +3,10 @@ package org.example.friendexam.service;
 import lombok.RequiredArgsConstructor;
 import org.example.friendexam.domain.Friend;
 import org.example.friendexam.repository.FriendRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,11 +27,25 @@ public class FriendService {
         return friendRepository.save(friend); // 저장해주고, 해당 객체를 리턴해줄 것
     }
 
-    // 친구 수정 - id에 해당하는 친구 정보 조회
+    // 친구 조회 - id에 해당하는 친구 정보 조회
     @Transactional(readOnly = true) // select니깐..
     public Friend findFriendById(Long id){
         return friendRepository.findById(id).orElse(null); // 만약 해당 id의 Friend가 없다면 null 객체 반환
     }
 
+    // 친구 삭제 - id에 해당하는 친구 삭제
+    @Transactional
+    public void deleteFriendById(Long id){
+        friendRepository.deleteById(id);
+    }
 
+    // 페이징 처리된 친구목록조회
+    public Page<Friend> findAllFriendsWithPage(Pageable pageable){
+        // id를 기준으로 desc
+        Pageable sortedByDescId =
+                PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by(Sort.Direction.DESC, "id"));
+
+        // Pagable 객체에서 지정한 것을 기준으로 findAll할 때 페이징 처리가 된다.
+        return friendRepository.findAll(sortedByDescId);
+    }
 }
